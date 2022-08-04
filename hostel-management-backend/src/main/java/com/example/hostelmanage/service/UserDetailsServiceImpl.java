b12b4e1e3fb62d260748e9b9d92d1abca03dc5ea
thanhvp <phuocthanh20cgh@gmail.com>
2022-08-05 00:29:47 +0700

    user detils servicer

--- hostel-management-backend/src/main/java/com/example/hostelmanage/service/UserDetailsServiceImpl.java
+++ hostel-management-backend/src/main/java/com/example/hostelmanage/service/UserDetailsServiceImpl.java
@@ -1,12 +1,12 @@
 package com.example.hostelmanage.service;
 
+import com.example.hostelmanage.model.Account;
 import com.example.hostelmanage.repository.AccountRepository;
-import org.springframework.security.core.userdetails.User;
+import com.example.hostelmanage.repository.RoleRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import com.example.hostelmanage.model.Account;
 
 import javax.inject.Inject;
 import java.util.Optional;
@@ -17,20 +17,34 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Inject
     private AccountRepository accountRepository;
 
+    @Inject
+    private RoleRepository roleRepository;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<Account> user =  accountRepository.findAccountByEmail(email);
         System.out.println("Find User: " + user.get().toString());
-        User.UserBuilder builder = null;
-        if (user.isPresent()) {
-            Account currentUser = user.get();
-            builder = org.springframework.security.core.userdetails.User.withUsername(email);
-            builder.password(currentUser.getPassword());
-            builder.roles(currentUser.getRole().getName());
-        } else {
-            throw new UsernameNotFoundException("User not found.");
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User is not found.");
         }
-
-        return builder.build();
+        return UserDetailsImpl.build(user.get());
     }
+
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        Optional<Account> user =  accountRepository.findAccountByEmail(email);
+//        System.out.println("Find User: " + user.get().toString());
+//        User.UserBuilder builder = null;
+//        if (!user.isPresent()) {
+//            Role role = roleRepository.findById();
+//
+//            builder = org.springframework.security.core.userdetails.User.withUsername(email);
+//            builder.password(currentUser.getPassword());
+//            builder.roles(currentUser.getRole().getName());
+//        } else {
+//            throw new UsernameNotFoundException("User is not found.");
+//        }
+//
+//        return builder.build();
+//    }
 }
